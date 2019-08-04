bde6d1f329c08d62e001c32c3e388a37d0871b99
Somnus <30816766+Somnus@users.noreply.github.com>
2019-08-04 12:47:55 +0800

    Add files via upload
    两个

---
+++ FXY_NetCore_WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FXY_NetCore_DbContext;
+using FXY_NetCore_DbEntity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace FXY_NetCore_WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class ValuesController : Controller
+    {
+        protected DefaultContext Context { get; set; }
+        public ValuesController(DefaultMySqlContext defaultMySqlContext, IMemoryCache cache)
+        {
+            Context = new DefaultContext(defaultMySqlContext, cache);
+        }
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+
+
+            Student student = new Student() { UUID = Guid.NewGuid().ToString(), Name = "成钢", BirthPlace = "西藏" };
+            Context.Add(student);
+            Context.SaveChanges();
+
+            Student student1 = Context.Get<Student>(student.UUID);
+
+            Thread.Sleep(10000);
+
+            Student student2 = Context.Get<Student>(student.UUID);
+
+            return new List<string>() { student.UUID };
+        }
+
+
+        public Student GetStudent(string uuid)
+        {
+            Student student = Context.Get<Student>(uuid);
+            return student;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ FXY_NetCore_WebApi/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FXY_NetCore_DbContext;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FXY_NetCore_WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMemoryCache();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<DefaultMySqlContext>(p =>
+                p.UseMySql("Server=127.0.0.1;Port=3306;Database=test;User=root;Password=root;sslmode=none;")
+            );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
